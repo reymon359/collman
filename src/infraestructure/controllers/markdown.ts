71d92b54c0154ce5ff28cbf9f30ca1f97b4b8f6f
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-09 21:45:07 +0100

    feat: start markdown controller

---
+++ src/infraestructure/controllers/markdown.ts
@@ -0,0 +1,7 @@
+// https://github.com/scottstanfield/markdown-to-json
+// https://github.com/IonicaBizau/json2md
+// https://github.com/scottstanfield/markdown-to-json
+
+export const getMarkdownCollection = (path:string) => {
+
+}
