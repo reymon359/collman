88eddcbab2fd3f4877cdc051635bb7395fbdb156
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-09 18:38:05 +0100

    feat: create first entities

---
+++ src/domain/entities/index.ts
@@ -0,0 +1,22 @@
+export interface Classification {
+  name: string // Example: Lets classify in Categories
+  values: [string] // Example: Array of the different Categories
+}
+
+export interface Item {
+  name: string // Name of the item
+  description: string // Description of the item
+  classifications: [Classification] // Array of different classifications
+  content: string // Main content of the item
+  dateCreated?: string
+  dateUpdated?: string
+}
+export interface Content {
+  name: string // Name of the collection content. Example: Posts or Animals
+  items: [Item] // Array of the Content items
+}
+export interface Index {
+  name: string
+  description: string
+  content: Content
+}
