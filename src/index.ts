ea6ab50e53f08253e601faf88c5024890a5add34
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-10 22:27:25 +0100

    chore: changes after talk with @adrian-afergon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { defaultConfiguration } from './configuration'
 const jdown = require('jdown')
+
 // TODO: Check if there is a custom config in the root directory and load it instead of the default
 export const collectionConfiguration = defaultConfiguration
 
