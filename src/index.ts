d45964afa8dbb4eafd689fcbf8b799a4080d93c8
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-13 11:00:28 +0100

    refactor: add main function to index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import { defaultConfiguration } from './configuration'
-const jdown = require('jdown')
+import { controllers } from './infraestructure/controllers'
 
 // TODO: Check if there is a custom config in the root directory and load it instead of the default
 export const collectionConfiguration = defaultConfiguration
 
-// Returns the inputCollection from the root directory specified in the config
-export const getInputCollection = async (configuration = collectionConfiguration) => {
-  const { pathRootDirectory /*, inputType */ } = configuration
-
-  const inputCollection = await jdown(pathRootDirectory)
-
-  return inputCollection
+const main = async () => {
+  const configuration = collectionConfiguration
+  // Get the config values needed
+  const { pathRootDirectory, inputType, outputType } = configuration
+  // Upload the collection, pass it to json
+  const collectionUploaded = await controllers.collection.uploadCollection(pathRootDirectory, inputType)
+  // Process it to the output specified in the config
+  const collectionProcessed = controllers.collection.processCollection(collectionUploaded, outputType)
+  // Save it in the folder specified in the config
+  controllers.collection.saveCollection(collectionProcessed, configuration)
 }
 
-// Process the collection WIP
-// export const processCollection = inputCollection => {
-//   const outputCollection
-//
-//   return outputCollection
-// }
+main()
