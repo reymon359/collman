2885c98627ef0eba74ba57a3062c37c870a88672
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-13 09:31:29 +0100

    feat: add id to models

--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -1,9 +1,11 @@
 export interface Classification {
+  id: string
   name: string // Example: Lets classify in Categories
   values: [string] // Example: Array of the different Categories
 }
 
 export interface Item {
+  id: string
   name: string // Name of the item
   description: string // Description of the item
   classifications: [Classification] // Array of different classifications
@@ -13,11 +15,13 @@ export interface Item {
 }
 
 export interface Content {
+  id: string
   name: string // Name of the collection content. Example: Posts or Animals
   items: [Item] // Array of the Content items
 }
 
 export interface Collection {
+  id: string
   name: string
   description: string
   content: Content
