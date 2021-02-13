509c31ddb55a27b4f1d987beb559602b7ed31aee
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-13 11:01:42 +0100

    refactor: update repositories

---
+++ src/infraestructure/repositories/index.ts
@@ -0,0 +1,4 @@
+export const repositories: any = {
+  markdown: {
+  }
+}
--- src/infraestructure/repositories/markdown.ts
+++ src/infraestructure/repositories/markdown.ts
@@ -1,7 +1,15 @@
 // https://github.com/scottstanfield/markdown-to-json
 // https://github.com/IonicaBizau/json2md
 // https://github.com/scottstanfield/markdown-to-json
+const jdown = require('jdown')
 
-export const getMarkdownCollection = (path:string) => {
+// Returns the json object
+export const getCollection = async (path:string) => {
+  const jsonCollection = await jdown(path)
+  return jsonCollection
+}
+
+// Creates a markdown directory from a json object
+export const createMarkdownCollection = (json:object, outputPath?:string) => {
 
 }
