3724cceb65e3fcbea0a274f15ded25da4ea3493b
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-14 21:14:28 +0100

    refactor: update index naming and structure

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,5 @@
-import { collectionConfiguration, getInputCollection } from './index'
+import { collectionConfiguration } from './index'
 import { defaultConfiguration } from './configuration'
-import defaultCollectionJson from './test/mocks/defaultCollection/defaultCollection.json'
 
 describe('Configuration', () => {
   it('loads the default configuration', () => {
@@ -10,40 +9,5 @@ describe('Configuration', () => {
     // Assert
     expect(loadedConfiguration).toEqual(defaultConfiguration)
     expect(loadedConfiguration).not.toBe({})
-  })
-})
-
-describe('Input System', () => {
-  it('gets the default input collection', async () => {
-    // Arrange
-    const mockedCollection = defaultCollectionJson
-    const mockedRootPath = 'src/test/mocks/defaultCollection/input/markdown/defaultCollection'
-    // Act
-    const inputCollection = await getInputCollection({ ...defaultConfiguration, pathRootDirectory: mockedRootPath })
-    // Assert
-    expect(inputCollection).toEqual(mockedCollection)
-  })
-
-  it('creates a docs folder', () => {
-
-  })
-})
-it.skip('process the collection properly', () => {
-  // WIP
-  // // Arrange
-  // const mockedInputCollection = null
-  // const mockedOutputCollection = null
-  //
-  // // Act
-  // const outputCollection = processCollection(mockedInputCollection)
-  //
-  // // Assert
-  // expect(outputCollection).toEqual(mockedOutputCollection)
-})
-
-describe.skip('Classification', () => {
-  it('gets the classifications', () => {
-  })
-  it('generates classifications folders', () => {
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ const main = async () => {
   // Get the config values needed
   const { pathRootDirectory, inputType, outputType } = configuration
   // Upload the collection, pass it to json
-  const collectionUploaded = await controllers.collection.uploadCollection(pathRootDirectory, inputType)
+  const collectionJson = await controllers.collection.getCollectionJson(pathRootDirectory, inputType)
   // Process it to the output specified in the config
-  const collectionProcessed = controllers.collection.processCollection(collectionUploaded, outputType)
+  const collectionProcessed = controllers.collection.processCollection(collectionJson, outputType)
   // Save it in the folder specified in the config
   controllers.collection.saveCollection(collectionProcessed, configuration)
 }
