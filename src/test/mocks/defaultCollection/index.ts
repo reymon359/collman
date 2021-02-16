d094105873857d042d97852dcbb6118070725362
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-16 08:01:41 +0100

    test: keep working on default collection mock

--- src/test/mocks/defaultCollection/index.ts
+++ src/test/mocks/defaultCollection/index.ts
@@ -1,8 +1,24 @@
-
+import { Item } from '../../../domain/models'
+// TODO: finish defaultCollectionMock
 export const defaultCollectionMock = {
   name: 'Default Collection',
   description: 'Default Collection description',
   content: {
-
+    name: 'items',
+    items: [
+      {
+        name: "item1",
+        description: "description1",
+        classifications: [{
+    name: "categories",
+    values: [
+      "Category1",
+      "Category2"
+    ]
+  }
+        ],
+        content: "<h1 id=\"title-1\">Title 1</h1>\n<p><img src=\"image-1.jpg\" alt=\"Image 1\"></p>\n<p>Item 1 introduction</p>\n"
+      }
+    ]
   }
 }
