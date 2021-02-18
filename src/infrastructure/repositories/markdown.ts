f648df3bd4d046235a56080b5bcd5bbf86ff7c8e
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-18 21:31:24 +0100

    feat: get index and items properly

--- src/infrastructure/repositories/markdown.ts
+++ src/infrastructure/repositories/markdown.ts
@@ -8,7 +8,7 @@ export interface JsonItem {
 }
 
 export interface JsonCollection {
-  jsonItems: [JsonItem]
+  items: [JsonItem]
 }
 
 
@@ -17,16 +17,71 @@ export const transformMarkdownDirectoryToJson = async (path:string,) => {
   return jsonCollection
 }
 
-export const transformJsonCollectionToCollection = async (jsonCollection:{}) => {
-  const collection = jsonCollection
+export const getClassificationsFromItems = async (items:any) => {
+  const classifications: [] = []
+  return classifications
+}
 
-  return collection
+
+export const transformInputDirectoryJsonToCollection = async (inputDirectoryJson: {}, inputDirectory:string) => {
+  // @ts-ignore
+  const jsonCollection  = inputDirectoryJson[inputDirectory]
+  const { index, ...jsonCollectionItems } = jsonCollection
+  console.log(jsonCollection, index, jsonCollectionItems)
+
+  const items = Object.keys(jsonCollectionItems).map(key =>  jsonCollectionItems[key]).map(({ contents: content, ...rest }) => ({ content, ...rest }));
+  console.log(items)
+
+//
+//   console.log(index, jsonItems)
+//   const items = jsonItems.map(({ contents: content, ...rest }) => ({ content, ...rest }));
+//   console.log(items)
+  const collectionClassifications = getClassificationsFromItems(items)
+// console.log('wtf')
+  // const collectionItems = processJsonItems
+
+
+  return {
+    name: index.name,
+    description: index.contents,
+    classifications: collectionClassifications,
+    content: {
+      name: inputDirectory,
+      items: [{
+        name: "item1",
+        description: "description1",
+        classifications: [{
+          name: "categories",
+          values: ["Category1", "Category2"]
+        }],
+        content: "# Title 1\n\n![Image 1](./assets/image-1.jpg)\n\nItem 1 introduction\n\n\n"
+      },{
+        name: "ITEM2",
+        description: "DESCRIPTION2",
+        classifications: [{
+          name: "categories",
+          values: ["Category1", "Category3"]
+        }],
+        content: "# Title 2\n\n![Image 2](./assets/image-2.png)\n\nItem 2 introduction\n\n\n"
+      },{
+        name: "Item3",
+        description: "Description3",
+        classifications: [{
+          name: "categories",
+          values: ["Category2", "Category3"]
+        }],
+        content: "# Title 3\n\n![Image 3](./assets/image-3.svg)\n\nItem 3 introduction\n\n\n"
+      }]
+    }
+
+
+  }
 }
 
 // Returns the json object
-export const getCollection = async (path:string) => {
-  const jsonCollection = await transformMarkdownDirectoryToJson(path)
-  const collection = await transformJsonCollectionToCollection(jsonCollection)
+export const getCollection = async (path:string,  inputDirectory:string) => {
+  const inputDirectoryJson = await transformMarkdownDirectoryToJson(path)
+  const collection = await transformInputDirectoryJsonToCollection(inputDirectoryJson, inputDirectory)
 
   return collection
 }
