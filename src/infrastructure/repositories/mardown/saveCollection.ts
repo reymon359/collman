458092d951e3c0b356bea1dc843db5d790e81d46
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-22 07:46:04 +0100

    feat: plan first approach

--- src/infrastructure/repositories/mardown/saveCollection.ts
+++ src/infrastructure/repositories/mardown/saveCollection.ts
@@ -1,4 +1,14 @@
+import { Configuration, defaultConfiguration } from '../../../configuration'
+import { Collection } from '../../../domain/models'
 
-export const saveCollection = async (path:string, outputDirectory:string) => {
+export const saveCollection = async (collection:Collection, configuration:Configuration = defaultConfiguration) => {
+  // 1. Create the output directory. Default docs
+
+  // 2. Create the index file
+
+  // 3. copy and paste the items in the folder removing the frontmatter. Or create the items with just the content and then copy the assets from the previous one
+
+  // 4. Generate the categories
+
   return 'TODO'
 }
