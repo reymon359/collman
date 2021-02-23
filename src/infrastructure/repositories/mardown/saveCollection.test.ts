b933f137fa712205d4bca81268dca110e793f3f2
reymon359 <ramonmorcillocascales@gmail.com>
2021-02-23 21:50:51 +0100

    feat: wip create directory test

--- src/infrastructure/repositories/mardown/saveCollection.test.ts
+++ src/infrastructure/repositories/mardown/saveCollection.test.ts
@@ -1,5 +1,6 @@
 import { defaultCollection } from '../../../test/mocks/defaultCollection'
-import { saveCollection } from './saveCollection'
+import { createOutputDirectory, saveCollection } from './saveCollection'
+const fs = require('fs')
 
 describe('Markdown repository save collection', () => {
   it.skip('saves a collection in a directory with markdown files', async () => {
@@ -11,5 +12,21 @@ describe('Markdown repository save collection', () => {
     // Assert
     expect('the docs folder').toEqual(mockedDefaultOutputCollection)
   })
+
+  it('creates the output directory', async () => {
+    const mockedPathRootDirectory = '/'
+    const mockedOutputDirectory = 'docs'
+
+    // Expect the docs folder to not exist
+    expect(fs.existsSync(mockedOutputDirectory)).toEqual(false)
+
+    await createOutputDirectory(mockedPathRootDirectory, mockedOutputDirectory)
+
+    // Expect the docs folder to exist
+    expect(fs.existsSync(mockedOutputDirectory)).toEqual(true)
+  })
 })
 // https://stackoverflow.com/questions/63499624/use-jest-to-compare-content-of-two-files
+// https://stackoverflow.com/questions/58810079/is-it-possible-to-write-jest-unit-tests-for-node-js-fs-readfile /
+// https://www.geeksforgeeks.org/node-js-fs-mkdir-method/
+// https://levelup.gitconnected.com/use-node-js-to-to-create-directories-and-files-734063ce93ec
--- src/infrastructure/repositories/mardown/saveCollection.ts
+++ src/infrastructure/repositories/mardown/saveCollection.ts
@@ -1,8 +1,28 @@
 import { Configuration, defaultConfiguration } from '../../../configuration'
 import { Collection } from '../../../domain/models'
+const fs = require('fs')
+const path = require('path')
+
+export const createOutputDirectory = (pathRootDirectory:string, outputDirectory:string) => {
+  // const outputDirectoryPath = `${pathRootDirectory}${outputDirectory}`
+
+  // fs.mkdir(outputDirectoryPath).then((err:any, data:any) => {
+  //   console.log(err,data)
+  // })
+
+  fs.mkdir(path.join(__dirname, outputDirectory),
+    { recursive: true }, (err:any) => {
+      if (err) {
+        return console.error(err)
+      }
+      console.log('Directory created successfully!')
+    })
+}
 
 export const saveCollection = async (collection:Collection, configuration:Configuration = defaultConfiguration) => {
+  const { pathRootDirectory, outputDirectory } = configuration
   // 1. Create the output directory. Default docs
+  await createOutputDirectory(pathRootDirectory, outputDirectory)
 
   // 2. Create the index file
 
