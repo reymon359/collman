6b207e9cf7b86071f21c8f061828830a373d522b
reymon359 <ramonmorcillocascales@gmail.com>
2021-03-02 07:45:04 +0100

    refactor: remove relative path and semicolon

--- src/infrastructure/repositories/mardown/saveCollection.ts
+++ src/infrastructure/repositories/mardown/saveCollection.ts
@@ -54,7 +54,7 @@ export const createClassifications = async (collection:Collection, outputDirecto
 
     // Create a file for each value
     for (const classificationValue of classification.values.sort()) {
-      const valueUrl = classificationValue.trim().replace(/\s/g, '%20');
+      const valueUrl = classificationValue.trim().replace(/\s/g, '%20')
       listOfValues.push({ link: { title: classificationValue, source: `${valueUrl}.md` } })
 
       const valueContentArray = []
@@ -68,7 +68,7 @@ export const createClassifications = async (collection:Collection, outputDirecto
           if (itemClassification.name === classification.name) {
             // If the items has values in that classification and includes the value
             if (itemClassification.values.length > 0 && itemClassification.values.includes(classificationValue)) {
-              listOfItemWithValue.push({ link: { title: item.name, source: `./${item.name}/index.md` } })
+              listOfItemWithValue.push({ link: { title: item.name, source: `${item.name}/index.md` } })
             }
           }
         })
