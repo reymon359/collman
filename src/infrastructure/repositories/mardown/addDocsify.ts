201cb1a8cdd3579cc2609fee275033247a47f2ba
reymon359 <ramonmorcillocascales@gmail.com>
2021-03-03 19:41:56 +0100

    feat: create add docsify methods structure

---
+++ src/infrastructure/repositories/mardown/addDocsify.ts
@@ -0,0 +1,34 @@
+import { Configuration, defaultConfiguration } from '../../../configuration'
+import { Collection } from '../../../domain/models'
+
+const createSidebarFile = async (outputDirectoryPath:string) => {
+  return 'todo'
+}
+
+const createIndexHtmlFile = async (outputDirectoryPath:string) => {
+  return 'todo'
+}
+
+const createNojekillFile = async (outputDirectoryPath:string) => {
+  return 'todo'
+}
+
+const createReadmeFile = async (outputDirectoryPath:string) => {
+  return 'todo'
+}
+
+export const addDocsify = async (collection:Collection, configuration:Configuration = defaultConfiguration) => {
+  const { pathRootDirectory, outputDirectory } = configuration
+  const outputDirectoryPath = `${pathRootDirectory}${outputDirectory}`
+  // 1. Copy the index and create a README.md with it
+  await createReadmeFile(outputDirectoryPath)
+
+  // 2. Create the .nojekill file
+  await createNojekillFile(outputDirectoryPath)
+
+  // 3. Create the index.html file
+  await createIndexHtmlFile(outputDirectoryPath)
+
+  // 4. Create the _sidebar.md file
+  await createSidebarFile(outputDirectoryPath)
+}
