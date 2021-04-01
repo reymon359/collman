de7b1ac50a1acb70dbc9c393ed5707df95c40294
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-01 09:15:34 +0200

    refactor: sort the contents of the sidebar too

--- src/infrastructure/repositories/mardown/addDocsify.ts
+++ src/infrastructure/repositories/mardown/addDocsify.ts
@@ -3,6 +3,7 @@ import { Collection } from '../../../domain/models'
 import { repositories } from '../index'
 import { getIndexHtmlContent } from './helpers/indexHtmlContent'
 import { urlifyString } from './helpers/urlifyString'
+import { sortUnorderedListOfLinks } from './helpers/sortUnorderedListOfLinks'
 const json2md = require('json2md')
 
 export interface DocsifyConfiguration {
@@ -32,7 +33,7 @@ const createSidebarFile = async (collection: Collection, outputDirectoryPath:str
   collection.content.items.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? 0 : -1)).forEach(item => {
     itemsUnorderedList.push({ link: { title: item.name, source: `${urlifyString(item.name)}/index.md` } })
   })
-  contentUnorderedList.push({ ul: itemsUnorderedList })
+  contentUnorderedList.push({ ul: sortUnorderedListOfLinks(itemsUnorderedList) })
   mainUnorderedList.push({ ul: contentUnorderedList })
 
   // Classifications
@@ -44,7 +45,7 @@ const createSidebarFile = async (collection: Collection, outputDirectoryPath:str
     classification.values.sort().forEach(value => {
       classificationValuesUnorderedList.push({ link: { title: value, source: `../${urlifyString(classification.name)}/${urlifyString(value)}.md` } })
     })
-    classificationUnorderedList.push({ ul: classificationValuesUnorderedList })
+    classificationUnorderedList.push({ ul: sortUnorderedListOfLinks(classificationValuesUnorderedList) })
     mainUnorderedList.push({ ul: classificationUnorderedList })
   })
 
