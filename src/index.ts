ce28af3fdee0778e479a362b9d0f41e22adc73cf
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-18 10:45:59 +0200

    refactor: improve configuration use

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,10 @@ import { Collection } from './domain/models'
 
 // TODO: Check if there is a custom config in the root directory and load it instead of the default
 const collman = async (configuration: Configuration = defaultConfiguration) => {
+  configuration = { ...defaultConfiguration, ...configuration }
   const collection = await controllers.collection.getCollection(configuration)
   // Process it to the output specified in the config
   await controllers.collection.saveCollection(collection as Collection, configuration)
 }
 
 export = collman
-// collman()
-// collman({ ...defaultConfiguration, pathRootDirectory: '/Users/ramonmorcillo/Documents/developer/collection-manager/src/test/mocks/defaultCollection/output/markdown/defaultCollectionWithDocsify/' })
