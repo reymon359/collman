293ed64843cbcde76a4b0f08688a01343b37862b
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-18 10:45:15 +0200

    refactor: improve logs

--- src/infrastructure/controllers/collection.ts
+++ src/infrastructure/controllers/collection.ts
@@ -5,12 +5,14 @@ import { Collection } from '../../domain/models'
 export const getCollection = async (configuration = defaultConfiguration): Promise<{}> => {
   const { inputType, inputDirectory, pathRootDirectory } = configuration
   const collection = await repositories[inputType].getCollection(pathRootDirectory, inputDirectory)
-  console.log('collection got:', collection)
+  console.log(`Collection got: ${collection.name}`)
   return collection
 }
 
 export const saveCollection = async (collection: Collection, configuration: Configuration) => {
   const { outputType } = configuration
+  console.log('Proceed to save collection')
   await repositories[outputType].saveCollection(collection, configuration)
+  console.log('Collection saved')
   if (configuration.docsify) await repositories[outputType].addDocsify(collection, configuration)
 }
