bbff9e6147ae9761d7caf6f5abe84996c4513026
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-20 18:15:54 +0200

    feat: add arguments

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+const yargs = require('yargs')
+
+const options = yargs
+  .usage('Usage: -n <name>')
+  .option('n', { alias: 'name', describe: 'Your name', type: 'string', demandOption: true })
+  .argv
+
+const greeting = `Hello, ${options.name}!`
+
+console.log(greeting)
