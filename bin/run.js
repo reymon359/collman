550e8528b30d368fe771fde17ba501e2ab855f24
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-20 19:40:23 +0200

    feat: use collman from lib index

---
+++ bin/run.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const collman = require('../lib/index')
+
+// inputType: string
+// outputType: string
+// pathRootDirectory: string // The root directory that holds the full project
+// inputDirectory: string // The directory with the original collection
+// outputDirectory: string // The output directory with the collection managed
+// docsify: boolean
+const options = yargs
+  .usage('Usage: -id <inputDirectory>')
+  .option('n', { alias: 'inputDirectory', describe: 'The directory with your collection', type: 'string', demandOption: true })
+  .argv
+
+collman({ inputDirectory: options.inputDirectory })
