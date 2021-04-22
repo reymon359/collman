9f760e6b6e183b10a4840a1b13b4529194bf6525
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-22 07:56:51 +0200

    feat: wip try to get the local config

--- src/infrastructure/controllers/collection.ts
+++ src/infrastructure/controllers/collection.ts
@@ -1,8 +1,21 @@
-import { Configuration } from '../../configuration'
+import { Configuration, defaultConfiguration } from '../../configuration'
 import { repositories } from '../repositories'
 import { Collection } from '../../domain/models'
+import path from 'path'
+
+const getLocalConfiguration = async ()=> {
+  console.log(__dirname)
+  // TODO: try this in the index file
+  const localConfigurationPath = path.join('./', 'collman.config.js')
+  console.log(localConfigurationPath)
+  const existsConfig = await repositories.fileSystem.pathExists(localConfigurationPath)
+  const localConfiguration = existsConfig ? require(localConfigurationPath) : {}
+  console.log(localConfiguration)
+  return localConfiguration
+}
 
 export const getCollection = async (configuration: Configuration): Promise<{}> => {
+  configuration = { ...defaultConfiguration, ...getLocalConfiguration() }
   const { inputType, inputDirectory, pathRootDirectory } = configuration
   const collection = await repositories[inputType].getCollection(pathRootDirectory, inputDirectory)
   console.log(`Collection got: ${collection.name}`)
