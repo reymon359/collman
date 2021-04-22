c05434e32f40da9cff3bfcfb9e7a0d6de082f086
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-22 21:07:23 +0200

    feat: get local config and improve logs

--- src/infrastructure/controllers/collection.ts
+++ src/infrastructure/controllers/collection.ts
@@ -4,28 +4,25 @@ import { Collection } from '../../domain/models'
 import path from 'path'
 
 const getLocalConfiguration = async () => {
-  console.log(__dirname)
-  // TODO: try this in the index file
-  const localConfigurationPath = path.join('./', 'collman.config.js')
-  console.log(localConfigurationPath)
+  const localConfigurationPath = path.join(process.cwd(), 'collman.config.js')
   const existsConfig = await repositories.fileSystem.pathExists(localConfigurationPath)
   const localConfiguration = existsConfig ? require(localConfigurationPath) : {}
-  console.log(localConfiguration)
   return localConfiguration
 }
 
 export const getCollection = async (configuration: Configuration): Promise<{}> => {
-  configuration = { ...defaultConfiguration, ...getLocalConfiguration() }
+  console.log('👀 Getting collection based on the configuration')
+  configuration = { ...defaultConfiguration, ...await getLocalConfiguration() }
   const { inputType, inputDirectory, pathRootDirectory } = configuration
   const collection = await repositories[inputType].getCollection(pathRootDirectory, inputDirectory)
-  console.log(`Collection got: ${collection.name}`)
+  console.log(`✅ Collection got: ${collection.name}`)
   return collection
 }
 
 export const saveCollection = async (collection: Collection, configuration: Configuration) => {
   const { outputType } = configuration
-  console.log('Proceed to save collection')
+  console.log(`🚧 Processing collection and saving in directory: ${configuration.outputDirectory}`)
   await repositories[outputType].saveCollection(collection, configuration)
-  console.log('Collection saved')
+  console.log('🎉 Collection processed and saved successfully')
   if (configuration.docsify) await repositories[outputType].addDocsify(collection, configuration)
 }
