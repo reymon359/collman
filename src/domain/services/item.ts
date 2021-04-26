cc0e282364d3f6b0fe964743d7e1146b24a00b9a
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-26 08:14:21 +0200

    refactor: remove unused services

--- src/domain/services/item.ts
+++
@@ -1,9 +0,0 @@
-// Posible Use Cases for the future
-
-export const getItem = () => 'todo'
-
-export const addItem = () => 'todo'
-
-export const updateItem = () => 'todo'
-
-export const removeItem = () => 'todo'
